89545320d14b3f05b8c687d1af8e1ae936aab9b1
Alex Shepherd <ashepherd2821@gmail.com>
2020-10-03 11:10:09 -0400

    add bullet

--- bullets/bullet.py
+++ bullets/bullet.py
@@ -1,5 +1,17 @@
 from .body import Body
 
+DEFAULT_EFFECTS = {"health": -0.1}
+
 
 class Bullet(Body):
-    pass
+    def __init__(self, pos, radius, color, effects=None):
+        super().__init__(pos=pos, radius=radius, color=color)
+        if effects is None:
+            effects = DEFAULT_EFFECTS
+
+        self.effects = effects
+
+    def hit_player(self, player):
+        if self.is_overlapping(player):
+            for key, value in self.effects.items():
+                player.attributes[key] += value
