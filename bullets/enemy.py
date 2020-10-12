5de2e5035afb5f623e9e9dba987c3d6dd402e810
Mohammad Ahmad Khan <41521621+deceptikhanqueror@users.noreply.github.com>
2020-10-12 18:54:49 -0500

    Update enemy.py
    fixed issues with previous commit

--- bullets/enemy.py
+++ bullets/enemy.py
@@ -1,28 +1,32 @@
 from .actor import Actor
 
+
 class Enemy(Actor):
-    def __init__(maxhealth, patterns, pcb1, pcb2, pcb3 )
+    def __init__(self, maxhealth, pcb1, pcb2, pcb3):
         self.health = maxhealth
         self.maxhealth = maxhealth
         self.phase = 0
-        self.status = "Alive"
         self.damage = 0
-  ## pcbs are the phase change boundaries (i.e. maxhealth>current_health>pcb1 is 1st (base) phase, pcb1>current_health>pcb2 is 2nd phase, etc. etc. etc.)
         self.pcb1 = pcb1
         self.pcb2 = pcb2
         self.pcb3 = pcb3
-  ## so what i was thinking here is that damage done to the boss is incremented per interval, and then reset every time the boss is updated - this way, everytime boss is updated,
-  ## damage done between updates is incremented to the health and phase, and then the damage is reset to zero for the new interval after having been incremented in the update
-  ## for the previous interval
-    def update(damage)
+        # pcbs are health thresholds for phase change
+
+    @property
+    def is_alive(self):
+        return self.health > 0
+
+    def update(self, damage):
+        ## so what i was thinking here is that damage done to the boss is incremented per interval, and then reset every time the boss is updated - this way, everytime boss is updated,
+        ## damage done between updates is incremented to the health and phase, and then the damage is reset to zero for the new interval after having been incremented in the update
+        ## for the previous interval
         self.health -= damage
         self.damage = 0
-        if  pcb1 < self.health <= maxhealth:
+        if self.pcb1 < self.health <= self.maxhealth:
             self.phase = 0
-        elif  pcb2 < self.health <= pcb1:
+        elif self.pcb2 < self.health <= self.pcb1:
             self.phase = 1
-        elif  pcb3 < self.health <= pcb2:
+        elif self.pcb3 < self.health <= self.pcb2:
             self.phase = 2
-        elif  0 < self.health <= pcb3:
+        elif 0 < self.health <= self.pcb3:
             self.phase = 3
-         
