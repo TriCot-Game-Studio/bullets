a01dd590244eebc4e3a1568b3f98eadd027c5a40
AdamSpannbauer <spannbaueradam@gmail.com>
2020-10-13 06:28:20 -0400

    please pycharm

--- bullets/enemy.py
+++ bullets/enemy.py
@@ -2,10 +2,10 @@ from .actor import Actor
 
 
 class Enemy(Actor):
-    def __init__(self, pos, radius, color, maxhealth, pcb1, pcb2, pcb3):
+    def __init__(self, pos, radius, color, max_health, pcb1, pcb2, pcb3):
         super().__init__(pos=pos, radius=radius, color=color)
-        self.health = maxhealth
-        self.maxhealth = maxhealth
+        self.health = max_health
+        self.max_health = max_health
         self.phase = 0
         self.damage = 0
         self.pcb1 = pcb1
@@ -18,12 +18,17 @@ class Enemy(Actor):
         return self.health > 0
 
     def update(self, damage):
-        ## so what i was thinking here is that damage done to the boss is incremented per interval, and then reset every time the boss is updated - this way, everytime boss is updated,
-        ## damage done between updates is incremented to the health and phase, and then the damage is reset to zero for the new interval after having been incremented in the update
-        ## for the previous interval
+        # so what i was thinking here is that damage
+        # done to the boss is incremented per interval,
+        # and then reset every time the boss is updated -
+        # this way, everytime boss is updated,
+        # damage done between updates is incremented to
+        # the health and phase, and then the damage is reset
+        # to zero for the new interval after having been
+        # incremented in the update for the previous interval
         self.health -= damage
         self.damage = 0
-        if self.pcb1 < self.health <= self.maxhealth:
+        if self.pcb1 < self.health <= self.max_health:
             self.phase = 0
         elif self.pcb2 < self.health <= self.pcb1:
             self.phase = 1
